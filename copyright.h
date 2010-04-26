acbf3af3e108a308a71d5254aeed36ea9ab222dc
Dave Dribin <dave@dribin.org>
2010-04-26 08:22:34 -0500

    Add copyright header

--- copyright.h
+++ copyright.h
@@ -21,26 +21,3 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
-#import <Foundation/Foundation.h>
-#import <AudioUnit/AudioUnit.h>
-#import <AudioToolbox/AudioToolbox.h>
-#import <AudioUnit/AudioUnit.h>
-#import "A440Player.h"
-#import "A440SineWaveGenerator.h"
-
-
-@interface A440AUGraph : NSObject <A440Player>
-{
-    AUGraph _graph;
-    AUNode _outputNode;
-    AUNode _converterNode;
-    AudioStreamBasicDescription _dataFormat;
-    
-    A440SineWaveGenerator _sineWaveGenerator;
-}
-
-- (BOOL)play:(NSError **)error;
-- (BOOL)stop:(NSError **)error;
-
-@end
